c4b69f2a843b02ed579fa42eb2257ca091a14d6b
Evren <evren.rahimoglu@gmail.com>
2021-05-11 13:38:18 +0300

    Object Oriented Programming Intro 2

---
+++ src/CourseManager.java
@@ -0,0 +1,11 @@
+
+public class CourseManager {
+	
+	public void buyCourse(Course course) {
+		System.out.println(course.getCourseName() + " kursunu " + course.getCoursePrice() + " TL ile satin aldiniz.");
+	}
+	
+	public void addCourse(Course course) {
+		System.out.println(course.getCourseName() + " kursu " + course.getCoursePrice() + " TL ile sisteme eklendi.");
+	}
+}
